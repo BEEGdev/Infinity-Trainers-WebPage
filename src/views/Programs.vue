f614f1eb82e529c56f79d1545bc32db8c6088a6c
beeg <beegdev13@gmail.com>
2024-03-22 00:55:22 +0100

    Avance en tarjetas responsive

--- src/views/Programs.vue
+++ src/views/Programs.vue
@@ -5,55 +5,60 @@ import FooterComponent from '../components/FooterComponent.vue';
 
 <template class="">
     <HeaderComponent/>
-    <img src="https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/6e815769a32d64e399e8e6df1121d4346d258a72edd67b9af4f637ef99f51071.jpg" alt="" class="w-full">
-    <div class="bg-[#016646] text-white p-8">
+    <div class="w-full h-64 bg-cover bg-center bg-[url('https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/6e815769a32d64e399e8e6df1121d4346d258a72edd67b9af4f637ef99f51071.jpg')]"> </div>
+    <div class="bg-[#016646] text-white p-8 text-center">
         <p class="p-1 font-extrabold text-2xl text-center">¿Estás dispuesto a apostar por ti?</p>
         <p class="p-7">
         Invertir en ti mismo es inestimable. En Infinity Trainers, más allá de un precio, ofrecemos un resultado. Diseñamos tarifas pensando en tus sueños, en esos retos personales que te has planteado. Porque entendemos que eres único, y tu entrenamiento debe ser tan especial como tú.
         </p>
     </div>
-        <div class="grid place-items-center my-10 w-full gap-20 drop-shadow-md" > <!--Tarjetas de bono-->
+        <div class="grid place-items-center my-10 w-full gap-20 drop-shadow-md lg:grid-flow-col lg:grid-cols-3 lg:gap-1 xl:gap-20 xl:w-10/12 xl:mx-auto" > <!--Tarjetas de bono-->
 
-            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white"> <!--Primer bono-->
+            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white sm:w-8/12 md:w-6/12 lg:w-8/12 xl:w-7/12"> <!--Primer bono-->
                 
-                    <h2 class="p-8 font-bold text-2xl   ">ESSENCE:<br>BONO 5 SESIONES</h2>
-                    <img src="https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/7c503a1b396b218d34b7f9953720f5a3d4befe13bacc523af79cdbaa209fe98a.jpg" alt="" class=" w-full">
-                    <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100]">
-                        <p class="bg-[#E4DBC0] font-semibold text-lg">
+                    <h2 class="p-8 font-bold text-2xl lg:text-xl lg:p-7 ">ESSENCE:<br>BONO 5 SESIONES</h2>
+                    <div class=" w-full bg-[url('https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/7c503a1b396b218d34b7f9953720f5a3d4befe13bacc523af79cdbaa209fe98a.jpg')] h-36 bg-cover bg-center md:h-40"></div>
+                    <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100] lg:p-3">
+                        <p class="bg-[#E4DBC0] font-semibold text-lg lg:text-sm">
                         Comienza en el entrenamiento personal por la puerta grande. 
                         En este paquete comenzarás un cambio que te guiará a conseguir los objetivos que te propongas
                         </p>
-                        <span class="font-extrabold text-lg">Desde 195 €</span>
+                        <span class="font-extrabold text-lg lg:text-base">Desde 195 €</span>
                     </div>
-                <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl">Quiero verlo</button>
+                <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl lg:text-base lg:p-4">Quiero verlo</button>
             </div>
 
-            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white drop-shadow-md"> <!--Segundo bono-->
+            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white md:w-9/12 lg:w-8/12 xl:w-7/12"> <!--Primer bono-->
                 
-                <h2 class="p-8 font-bold text-2xl">LIMITLESS: <br>BONO 10 SESIONES</h2>
-                <img src="https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/1abe314cd802166ac89fae60aa9413c7ba4ed5afc48c7b89c278481fcff7b539.jpg" alt="" class="h-full w-full">
-                <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100]">
-                    <p class="bg-[#E4DBC0] font-semibold text-lg">
-                        Diseñado para aquellos que buscan un gran cambio, un entrenamiento que te cambiará físicamente a pasos agigantados.
+                <h2 class="p-8 font-bold text-2xl lg:text-xl lg:p-7">ESSENCE:<br>BONO 5 SESIONES</h2>
+                <div class=" w-full bg-[url('https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/1abe314cd802166ac89fae60aa9413c7ba4ed5afc48c7b89c278481fcff7b539.jpg')] h-36 bg-cover bg-center"></div>
+                <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100] lg:p-3">
+                    <p class="bg-[#E4DBC0] font-semibold text-lg lg:text-sm">
+                    Comienza en el entrenamiento personal por la puerta grande. 
+                    En este paquete comenzarás un cambio que te guiará a conseguir los objetivos que te propongas
                     </p>
-                    <span class="font-extrabold text-lg">Desde 210 €</span>
+                    <span class="font-extrabold text-lg lg:text-base">Desde 195 €</span>
                 </div>
-            <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl">Quiero verlo</button>
+            <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl lg:text-base lg:p-4">Quiero verlo</button>
             </div>
 
-            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white drop-shadow-md"> <!--Tercer bono-->
+            <div class="w-10/12 bg-[#016646] text-center rounded-xl text-white md:w-9/12 lg:w-8/12 xl:w-7/12"> <!--Primer bono-->
                 
-                <h2 class="p-8 font-bold text-2xl">INFINITY ELITE <br>EXPERIENCE</h2>
-                <img src="https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/657187fb37458f2f3021ee3e4293a22b3852e6d08f026d5a33430b1b3806e9eb.jpg" alt="" class="h-full w-full">
-                <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100]">
-                    <p class="bg-[#E4DBC0] font-semibold text-lg">
-                        Nuestro paquete premium. Con todo un equipo de profesionales de la actividad física, nutricionistas y fisioterapeutas se coordinan para ofrecerte la transformación más increíble que hay en el sector.
+                <h2 class="p-8 font-bold text-2xl lg:text-xl lg:p-7">ESSENCE:<br>BONO 5 SESIONES</h2>
+                <div class=" w-full bg-[url('https://cloud1.email2go.io/7efeb6400e09756814e99049835fa47b/657187fb37458f2f3021ee3e4293a22b3852e6d08f026d5a33430b1b3806e9eb.jpg')] h-36 bg-cover bg-center"></div>
+                <div class="p-7 bg-[#E4DBC0] grid sm:grid-cols-1 gap-5 text-[#372100] lg:p-3">
+                    <p class="bg-[#E4DBC0] font-semibold text-lg lg:text-sm">
+                    Comienza en el entrenamiento personal por la puerta grande. 
+                    En este paquete comenzarás un cambio que te guiará a conseguir los objetivos que te propongas
                     </p>
-                    <span class="font-extrabold text-lg">Desde 800€/mes</span>
+                    <span class="font-extrabold text-lg lg:text-base">Desde 195 €</span>
                 </div>
-            <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl">Quiero verlo</button>
+            <button class="p-6 text-2xl font-semibold bg-[#A88A5E] w-full rounded-b-xl lg:text-base lg:p-4">Quiero verlo</button>
             </div>
+
+            
         </div>
 
     <FooterComponent/>
 </template>
+
